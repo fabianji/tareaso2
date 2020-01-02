32e3164b10b029ecc6d8932a040297b7e9a63ea5
zadage <zadage0023@gmail.com>
2020-01-02 16:08:03 -0300

    hace quicksort sin thread de arreglo aleatorio

--- Parte2/src/Main.java
+++ Parte2/src/Main.java
@@ -1,8 +1,59 @@
+import java.util.Random;
+
 public class Main {
+    public static int randomFill(int n){
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt(n*10 +1) +1;
+        return randomNum;
+    }
+    //n tamaño arreglo
+    public static int[] crear_arreglo_desordenado( int n){
+        int[] arreglo = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arreglo[i] = randomFill(n);
+        }
+        return arreglo;
+
+
+    }
+
+//http://puntocomnoesunlenguaje.blogspot.com/2012/12/java-quicksort.html
+    public static void quicksort(int A[], int izq, int der) {
+
+        int pivote=A[izq]; // tomamos primer elemento como pivote
+        int i=izq; // i realiza la búsqueda de izquierda a derecha
+        int j=der; // j realiza la búsqueda de derecha a izquierda
+        int aux;
+
+        while(i<j){            // mientras no se crucen las búsquedas
+            while(A[i]<=pivote && i<j) i++; // busca elemento mayor que pivote
+            while(A[j]>pivote) j--;         // busca elemento menor que pivote
+            if (i<j) {                      // si no se han cruzado
+                aux= A[i];                  // los intercambia
+                A[i]=A[j];
+                A[j]=aux;
+            }
+        }
+        A[izq]=A[j]; // se coloca el pivote en su lugar de forma que tendremos
+        A[j]=pivote; // los menores a su izquierda y los mayores a su derecha
+        if(izq<j-1)
+            quicksort(A,izq,j-1); // ordenamos subarray izquierdo
+        if(j+1 <der)
+            quicksort(A,j+1,der); // ordenamos subarray derecho
+    }
+
+
+
 
     public static void main(String[] args) {
+        //int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
 
 
-        System.out.println("Hello World!");
+        int[] arreglo = crear_arreglo_desordenado(30);
+        System.out.println(java.util.Arrays.toString(arreglo));
+        quicksort(arreglo , 0,29);
+        System.out.println(java.util.Arrays.toString(arreglo));
     }
 }
